1bc793ecfd70cbd6cd3d94a49a6f5761a9431004
jive <jive@yopmail.com>
2021-02-23 11:29:53 +0100

    environment apiUrl changed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://pfemback:3035'
+  apiUrl: 'pfemback'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,5 +5,5 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://pfemback:3035'
+  apiUrl: 'pfemback'
 };
