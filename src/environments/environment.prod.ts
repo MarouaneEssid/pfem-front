289ecb5e3d9e0098598d36ce07e383d81988b540
MarouaneEssid <marouane.essid@gmail.com>
2021-02-24 10:53:27 +0100

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://2886795375-3035-simba08.environments.katacoda.com:443'
+  apiUrl: 'http://localhost:3035'
+  // apiUrl: 'https://2886795375-3035-simba08.environments.katacoda.com:443'
 };
